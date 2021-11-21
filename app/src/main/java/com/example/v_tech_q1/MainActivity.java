a89875197139461f21ca2ccf167687680ca1f6e7
Aryaman <aryaman.awasthi79@gmail.com>
2021-11-21 11:21:23 +0530

    UI Upgrades

--- app/src/main/java/com/example/v_tech_q1/MainActivity.java
+++ app/src/main/java/com/example/v_tech_q1/MainActivity.java
@@ -16,18 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText editText = (EditText) findViewById(R.id.et);
 
-        Button btn = (Button) findViewById(R.id.buttonPanel);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String text = editText.getText().toString().replace(" ","").replace("\n","");
-                char[] arr = text.toCharArray();
-                Arrays.sort(arr);
-                TextView tv = (TextView)findViewById(R.id.tv);
-                tv.setText(Arrays.toString(arr));
-            }
-        });
+
     }
 }
